d51567fa2614768c05f78d587fad54ea92a27d70
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-25 14:58:51 +0300

    next: workspace alert

--- apps/web/components/ui/submit-button.tsx
+++ apps/web/components/ui/submit-button.tsx
@@ -1,20 +1,22 @@
 "use client"
 import { Button } from '@repo/ui/components/ui/button'
+import { cn } from '@repo/ui/lib/utils'
 import React, { PropsWithChildren, MouseEventHandler } from 'react'
 import { useFormStatus } from 'react-dom'
 
 interface SubmitButtonProps extends PropsWithChildren {
     onClick?: MouseEventHandler<HTMLButtonElement>
+    fullWidth?: boolean
 }
 
-const SubmitButton = ({ children, onClick }: SubmitButtonProps) => {
+const SubmitButton = ({ children, onClick, fullWidth = true }: SubmitButtonProps) => {
     const { pending } = useFormStatus()
 
     return (
         <Button
             type='submit'
             aria-disabled={pending}
-            className='w-full mt-2'
+            className={cn(fullWidth ? 'w-full' : '')}
             onClick={onClick}
         >
             {pending ? "Submitting..." : children}
