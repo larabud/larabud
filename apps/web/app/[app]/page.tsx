aa30980ad1308a719ef82f2f9b230b5c4cd59a67
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-11-08 14:40:18 +0300

    nextjs subdomain configuration

---
+++ apps/web/app/[app]/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+            <Button className="font-mono">App sub.</Button>
+        </>
+    );
+}
